c33b7a2ca9e1cd770b5c75b2fc846196d215ddd8
Mark Secrist <msecrist@pivotal.io>
2019-11-05 15:30:00 -0700

    start point for 01-Overview lab

---
+++ customer-web/src/test/java/io/pivotal/bookshop/CustomerWebApplicationTests.java
@@ -0,0 +1,18 @@
+package io.pivotal.bookshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("local")
+public class CustomerWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
